9a7492c0c00cf4eccd92fd0c506886c7dcf1d072
Darent98 <jannik.krueger1998@gmail.com>
2024-07-22 08:43:17 +0200

    fix: 🐛 oauth registration did not work

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,15 +5,15 @@ class PasswordsController < Devise::PasswordsController
   # Needed for OAuth users (otherwise an already logged in user wouldn't be able to reset their password)
   skip_before_action :require_no_authentication, :only => [:edit, :update]
 
-#   def update
-#     super
+  # def update
+  #   super
 
-#     if resource.errors.empty?
-#       resource.update!(has_set_password: true) unless resource.has_set_password?
+  #   if resource.errors.empty?
+  #     resource.update!(has_set_password: true) unless resource.has_set_password?
 
-#       # See: https://stackoverflow.com/a/22110985/1857435
-#       sign_out(resource_name)
-#       sign_in(resource_name, resource)
-#     end
-#   end
+  #     # See: https://stackoverflow.com/a/22110985/1857435
+  #     sign_out(resource_name)
+  #     sign_in(resource_name, resource)
+  #   end
+  # end
 end
